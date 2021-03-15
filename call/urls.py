14877376251b1167e0be03a9ca794de4691563bf
TeslaLord <ntharun@gmail.com>
2021-03-16 00:04:21 +0530

    connecting heroku

--- call/urls.py
+++ call/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/advisor', user_views.add_advisor),
     path('user/',include('user.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
